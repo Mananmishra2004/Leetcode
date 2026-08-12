771204d3fd7e08349300378826e510e903e74c81
Manan Mishra <137518804+Mananmishra2004@users.noreply.github.com>
2026-08-12 22:18:36 +0530

    Time: 1 ms (77.46%) | Memory: 42.4 MB (51.44%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,29 @@
+class Solution {
+    int fun(int n){
+        int sum =0;
+
+        while(n>0){
+            int digit = n%10;
+            sum += digit * digit;
+            n = n/10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        while(true){
+            slow = fun(slow);
+            fast = fun(fun(fast));
+
+            if(fast == 1){
+                return true;
+            }
+
+            if(fast == slow){
+                return false;
+            }
+        }
+    }
+}
